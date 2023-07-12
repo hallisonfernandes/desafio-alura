925a8d3d4a3d5d540c31c4e01c662cd26fd496be
Hallison Fernandes <133378183+hallisonfernandes@users.noreply.github.com>
2023-07-12 20:59:02 -0300

    desafio dia 1

---
+++ dia1/script.js
@@ -0,0 +1,29 @@
+let numeroUm = Number(1)
+//alert(typeof(numeroUm))
+let stringUm = '1'
+//alert(typeof(stringUm))
+let numeroTrinta = Number(39)
+let stringTrinta = '30'
+let numeroDez = Number(10)
+let stringDez = '10'
+let res = document.querySelector('div#res')
+
+if (numeroUm == stringUm && typeof(numeroUm) !== typeof(numeroUm)){
+    res.innerHTML = (`- As variáveis <strong>${numeroUm}</strong> do tipo <strong>${typeof(numeroUm)}</strong> e <strong>${stringUm}</strong> do tipo <strong>${typeof(stringUm)}</strong> tem o mesmo valor, mas tipos diferentes <br>`)
+} else if (numeroUm !== stringUm) {
+    res.innerHTML = (`- As variáveis <strong>${numeroUm}</strong> do tipo <strong>${typeof(numeroUm)}</strong> e <strong>${stringUm}</strong> do tipo <strong>${typeof(stringUm)}</strong> não tem o mesmo valor <br>`)
+}
+
+if (numeroTrinta == stringTrinta && typeof(numeroTrinta) == typeof(stringTrinta)) {
+    res.innerHTML += (`- As variáveis <strong>${numeroTrinta}</strong> do tipo <strong>${typeof(numeroTrinta)}</strong> e <strong>${stringTrinta}</strong> do tipo <strong>${typeof(stringTrinta)}</strong> tem o mesmo valor e mesmo tipo <br>`)
+} else if (numeroTrinta == stringTrinta && typeof(numeroTrinta) !== typeof(stringTrinta)){
+    res.innerHTML += (`- As variáveis <strong>${numeroTrinta}</strong> do tipo <strong>${typeof(numeroTrinta)}</strong> e <strong>${stringTrinta}</strong> do tipo <strong>${typeof(stringTrinta)}</strong> tem o mesmo valor, mas não tem o mesmo tipo <br>`)
+} else if (numeroTrinta !== stringTrinta && typeof(numeroTrinta) !== typeof(stringTrinta)) {
+    res.innerHTML += (`- As variáveis <strong>${numeroTrinta}</strong> do tipo <strong>${typeof(numeroTrinta)}</strong> e <strong>${stringTrinta}</strong> do tipo <strong>${typeof(stringTrinta)}</strong> não tem o mesmo valor e não tem o mesmo tipo <br>`)
+}
+
+if (numeroDez == stringDez && typeof(numeroDez) !== typeof(stringDez)){
+    res.innerHTML += (`- As variáveis <strong>${numeroDez}</strong> do tipo <strong>${typeof(numeroDez)}</strong> e <strong>${stringDez}</strong> do tipo <strong>${typeof(stringDez)}</strong> tem o mesmo valor, mas tipos diferentes <br>`)
+} else if (numeroDez !== stringDez) {
+    res.innerHTML += (`- As variáveis <strong>${numeroUm}</strong> do tipo <strong>${typeof(numeroUm)}</strong> e <strong>${stringUm}</strong> do tipo <strong>${typeof(stringUm)}</strong> não tem o mesmo valor <br>`)
+}
